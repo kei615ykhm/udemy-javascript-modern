7ee9e999b8014a8ab4c6328b0937b355e51930fa
kei615 <from0615.ykhm@gmail.com>
2024-08-21 22:12:44 +0900

    add: 関数を`import`で取り込む記述

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
-import profile from "./profile.js";
+// utils.jsに存在するspuare関数をインポートして利用する
+// ｛｝の意味は、utils.jsに存在する関数を全てインポートするという意味
+import { spuare } from "./utils.js";
 
-console.log(profile);
+console.log(spuare(5)); // 25
---
+++ utils.js
@@ -0,0 +1,3 @@
+export const square = (length) => {
+  return length * length;
+};
