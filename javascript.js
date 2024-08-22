1dfd6e8cf288476c7cf356d5d58fa741d8fa6aaa
kei615 <from0615.ykhm@gmail.com>
2024-08-22 18:23:50 +0900

    add: 関数の引数でオブジェクトの分割代入をする

--- javascript.js
+++ javascript.js
@@ -10,3 +10,13 @@ console.log(nickname); // Ham
 
 // nicknameプロパティ以外のプロパティがオブジェクトとしてrestに代入されている
 console.log(rest); // { age: 45 }
+
+// オブジェクトの分割代入は、関数の引数にも使える
+
+const printObject = (profile) => {
+  // 引数profileのnicknameプロパティの値を取り出して表示(.はオブジェクトのプロパティにアクセスする演算子)
+  // この場合、profileというオブジェクトのnicknameというプロパティにアクセスしている
+  console.log(`nickname: ${profile.nickname}`);
+};
+
+printObject(profile); // nickname: Ham
