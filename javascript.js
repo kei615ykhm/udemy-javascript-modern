7259f184e667b6dbe7b5c36e778bbdb0237e5cfd
kei615 <from0615.ykhm@gmail.com>
2024-08-22 22:16:17 +0900

    test; 処理にかかる時間を計測する
    - 平均して`90ms`は待たされてしまう

--- javascript.js
+++ javascript.js
@@ -2,10 +2,14 @@
 const request = new XMLHttpRequest();
 console.log(1);
 // リクエストの初期設定(メソッド、URL、非同期通信のON/OFFという三つの設定を行う)
-request.open("GET", "https://jsonplaceholder.typicode.com/photos", true);
+request.open("GET", "https://jsonplaceholder.typicode.com/photos", false);
 console.log(2);
+const before = new Date();
 // リクエストの送信(nullを引数にして送信)
 request.send(null);
+const after = new Date();
+const ms = after - before;
+console.log(`${ms}ms`);
 console.log(3);
 
 // ネットワークのphotosのリクエストを確認
@@ -16,7 +20,7 @@ console.log(3);
 // レスポンスの確認
 if (request.status === 200) {
   // レスポンスの表示
-  console.log(request.response);
+  //   console.log(request.response);
 }
 
 console.log(4);
