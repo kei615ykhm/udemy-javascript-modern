24eec9d463a08bc423983a000db18b01e5154da3
kei615 <from0615.ykhm@gmail.com>
2024-08-22 22:37:51 +0900

    test: `setTimeout`を使って非同期処理のテストをする(2)
    - `console.log`に1秒経ったよという文字を入れて視覚的にわかりやすくした

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
 console.log(1);
 // settimeoutというapiを使って、1秒後にコンソールに2を表示する
 setTimeout(() => {
-  console.log(2);
+  console.log("1秒経ったよ");
 }, 1000);
 
 console.log(3);
 
-// 1→3→2と表示される
+// 1→3→1秒経ったよと表示される
