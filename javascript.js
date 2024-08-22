13da8920b8125643f8e2e0f68c68f03d7a5a47a1
kei615 <from0615.ykhm@gmail.com>
2024-08-22 22:08:17 +0900

    add: 上から順番に実行されているかのテスト

--- javascript.js
+++ javascript.js
@@ -1,9 +1,12 @@
 // リクエストオブジェクトの生成
 const request = new XMLHttpRequest();
+console.log(1);
 // リクエストの初期設定(メソッド、URL、非同期通信のON/OFFという三つの設定を行う)
 request.open("GET", "https://jsonplaceholder.typicode.com/photos", true);
+console.log(2);
 // リクエストの送信(nullを引数にして送信)
 request.send(null);
+console.log(3);
 
 // ネットワークのphotosのリクエストを確認
 // リクエスト URL:https://jsonplaceholder.typicode.com/photos
@@ -15,3 +18,5 @@ if (request.status === 200) {
   // レスポンスの表示
   console.log(request.response);
 }
+
+console.log(4);
