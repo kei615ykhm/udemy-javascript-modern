167ee77fdffb82e8aa9a092d688aca98ee992703
kei615 <from0615.ykhm@gmail.com>
2024-08-22 22:01:57 +0900

    add: リクエストオブジェクトの生成と初期設定

--- javascript.js
+++ javascript.js
@@ -1,18 +1,11 @@
-// 同期処理の原則
+// リクエストオブジェクトの生成
+const request = new XMLHttpRequest();
+// リクエストの初期設定(メソッド、URL、非同期通信のON/OFFという三つの設定を行う)
+request.open("GET", "https://jsonplaceholder.typicode.com/photos", true);
+// リクエストの送信(nullを引数にして送信)
+request.send(null);
 
-// 1. 関数の実行が終わるまで次の処理に進まない
-// 2. 上から順番に処理される
-
-const a = 1;
-const b = 2;
-
-console.log(a + b); // 3
-
-// なぜここで3が出力されるのか？
-// 1. const a = 1; が実行される
-// 2. const b = 2; が実行される
-// 3. console.log(a + b); が実行される
-// 4. 1 + 2 が実行される
-// 5. 3 が出力される
-
-// このように、同期処理の原則により、上から順番に処理される
+// ネットワークのphotosのリクエストを確認
+// リクエスト URL:https://jsonplaceholder.typicode.com/photos
+// リクエスト メソッド:GET
+// ステータス コード:200 OK
