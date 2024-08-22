ff8bbe40b52b174ba117a53a08068367550dd9b3
kei615 <from0615.ykhm@gmail.com>
2024-08-22 21:02:57 +0900

    add: `map`メソッドを使って配列のデータに対して２乗の掛け算を実行させる

--- javascript.js
+++ javascript.js
@@ -1,21 +1,14 @@
-//オブジェクトの分割代入
+// mapメソッド
 
-// ｛｝の中にオブジェクトを入れると、オブジェクトのプロパティを変数として取り出すことができる
-const profile = { nickname: "Ham", age: 45 };
-// restにはnicknameプロパティ以外のプロパティが代入される
-const { nickname, ...rest } = profile;
+// mapメソッドとは、配列のデータに対して実行できる関数
+// ある配列のすべての要素に対して、関数を実行し、その結果を新しい配列として返す
 
-// nicknameプロパティの値が変数nicknameに代入されている
-console.log(nickname); // Ham
+// mapメソッドの基本構文
+const numbers = [5, 3, 4];
+// mapメソッドを使って、numbers配列の各要素を2倍にして新しい配列を作成
+const squares = numbers.map((number) => {
+  // returnで返す値が新しい配列の要素になる
+  return number * number;
+});
 
-// nicknameプロパティ以外のプロパティがオブジェクトとしてrestに代入されている
-console.log(rest); // { age: 45 }
-
-// オブジェクトの分割代入は、関数の引数にも使える
-
-// nicknameだけを引数として受け取る場合は、引数にオブジェクトを指定して、その中でnicknameプロパティを分割代入する
-const printObject = ({ nickname }) => {
-  console.log(`nickname: ${nickname}`);
-};
-
-printObject(profile); // nickname: Ham
+console.log(squares); // [25, 9, 16]
