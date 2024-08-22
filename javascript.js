efbe03b0a3c3511e0851a8b7ac7c2a95979726b9
kei615 <from0615.ykhm@gmail.com>
2024-08-22 17:28:06 +0900

    update: 分割代入の書き方
    - ひとつの式で複数の変数を代入することができる

--- javascript.js
+++ javascript.js
@@ -1,13 +1,9 @@
-// 配列の代入
+// 配列の分割代入
 
 // []の中に変数を入れることで、配列の中身を変数に代入できる
-const array = ["Ham", 35];
-// nicknameには"Ham"が代入される
-const nickname = array[0];
-// ageには35が代入される
-const age = arrar[1];
-
-// 配列では0から始まるので、0番目の要素を取得するときはarray[0]とする
+const array = ["Ham", 45];
+// ひとつの式で複数の変数に代入することができる。これが分割代入の仕組み
+const [nickname, age] = array;
 
 console.log(nickname); // Ham
-console.log(age); // 35
+console.log(age); // 45
