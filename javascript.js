9217811aed9130381521ec942eda9bdf75d28e4a
kei615 <from0615.ykhm@gmail.com>
2024-08-22 22:36:02 +0900

    test: `setTimeout`を使って非同期処理のテストをする

--- javascript.js
+++ javascript.js
@@ -1,22 +1,9 @@
-// リクエストオブジェクトの生成
-const request = new XMLHttpRequest();
 console.log(1);
-// リクエストの初期設定(メソッド、URL、非同期通信のON/OFFという三つの設定を行う)
-request.open("GET", "https://jsonplaceholder.typicode.com/photos", true);
-console.log(2);
-request.onload = function () {
-  console.log(3);
-  // request.statusのチェックをここに移植する
-  if (request.status === 200) {
-    //   console.log(request.response);
-  }
-};
+// settimeoutというapiを使って、1秒後にコンソールに2を表示する
+setTimeout(() => {
+  console.log(2);
+}, 1000);
 
-const before = new Date();
-// リクエストの送信(nullを引数にして送信)
-request.send(null);
-const after = new Date();
-const ms = after - before;
-console.log(`${ms}ms`);
+console.log(3);
 
-console.log(4);
+// 1→3→2と表示される
