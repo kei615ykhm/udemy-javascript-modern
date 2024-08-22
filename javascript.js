42d59fa3ee53c7dbcef9c546d03d5a6d34b16ff4
kei615 <from0615.ykhm@gmail.com>
2024-08-22 22:31:40 +0900

    update: 非同期処理（コールバック）への移行
    - `send`が同期処理から非同期処理に変更したら`90ms`から`0ms`になった

--- javascript.js
+++ javascript.js
@@ -2,25 +2,21 @@
 const request = new XMLHttpRequest();
 console.log(1);
 // リクエストの初期設定(メソッド、URL、非同期通信のON/OFFという三つの設定を行う)
-request.open("GET", "https://jsonplaceholder.typicode.com/photos", false);
+request.open("GET", "https://jsonplaceholder.typicode.com/photos", true);
 console.log(2);
+request.onload = function () {
+  console.log(3);
+  // request.statusのチェックをここに移植する
+  if (request.status === 200) {
+    //   console.log(request.response);
+  }
+};
+
 const before = new Date();
 // リクエストの送信(nullを引数にして送信)
 request.send(null);
 const after = new Date();
 const ms = after - before;
 console.log(`${ms}ms`);
-console.log(3);
-
-// ネットワークのphotosのリクエストを確認
-// リクエスト URL:https://jsonplaceholder.typicode.com/photos
-// リクエスト メソッド:GET
-// ステータス コード:200 OK
-
-// レスポンスの確認
-if (request.status === 200) {
-  // レスポンスの表示
-  //   console.log(request.response);
-}
 
 console.log(4);
