23e0dc706167599c0668f99721b5f403da968a70
kei615 <from0615.ykhm@gmail.com>
2024-08-24 00:23:19 +0900

    update: `promise`に`setTimeout`を使ってみる

--- javascript.js
+++ javascript.js
@@ -2,19 +2,22 @@
 const Promise = new Promise((resolve, reject) => {
   // 時間のかかる処理
   // resolve("成功");
+  setTimeout(() => {
+    console.log("1秒経ったよ");
+  }, 1000);
 });
 console.log(promise);
 
 console.log("start");
 
-setTimeout(() => {
-  console.log("1秒経ったよ");
-  setTimeout(() => {
-    console.log("1秒経ったよ");
-    setTimeout(() => {
-      console.log("1秒経ったよ");
-    }, 1000);
-  }, 1000);
-}, 1000);
+// setTimeout(() => {
+//   console.log("1秒経ったよ");
+//   setTimeout(() => {
+//     console.log("1秒経ったよ");
+//     setTimeout(() => {
+//       console.log("1秒経ったよ");
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
 
 console.log("end");
