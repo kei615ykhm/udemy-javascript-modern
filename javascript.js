c3033611046c7bfe161005462b0c1c9967ce2859
kei615 <from0615.ykhm@gmail.com>
2024-08-25 15:45:43 +0900

    test: `Promise`instanceの状態を取得するテスト

--- javascript.js
+++ javascript.js
@@ -9,6 +9,9 @@ const Promise = new Promise((resolve, reject) => {
 });
 console.log(promise);
 
+// Promiseインスタンスの状態を取得するメソッド
+console.log(promise.isFulfilled()); // false
+
 // thenメソッドで成功時の処理を記述(resolveが実行されたときに呼ばれる)
 // 副作用として、非同期処理の結果を取得することができる
 promise.then(() => {
