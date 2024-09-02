bb32a22b996c4943b3688886d7567ffc53ea88d1
kei615 <from0615.ykhm@gmail.com>
2024-09-02 20:43:34 +0900

    update: 後続の非同期処理を順番につなげて実行させる

--- javascript.js
+++ javascript.js
@@ -1,22 +1,20 @@
 console.log("start");
 
-const sleep = () => {
+const sleep = (milliseconds) => {
   return new Promise((resolve) => {
     setTimeout(() => {
-      console.log("1秒経ったよ");
+      console.log(`${milliseconds} ms`);
       resolve();
-    }, 1000);
+    }, milliseconds);
   });
 };
 
-const promise = sleep();
-
-promise
-  .then(() => sleep())
-  .then(() => sleep())
-  .then(() => sleep())
-  .then(() => sleep())
-  .then(() => sleep())
-  .then(() => sleep());
+sleep(900)
+  .then(() => sleep(901))
+  .then(() => sleep(902))
+  .then(() => sleep(903))
+  .then(() => sleep(904))
+  .then(() => sleep(905))
+  .then(() => sleep(906));
 
 console.log("end");
